14700ee07ee9f10eeb682209331afd9601b09719
irfangg <ir786.g@gmail.com>
2021-07-22 14:30:35 +0530

    fixed eslint errors

--- src/chrome/content.js
+++ src/chrome/content.js
@@ -1,13 +1,14 @@
 /*global chrome */
-console.log('content script')
+console.log('content script');
 
+// eslint-disable-next-line no-unused-vars
 const messagesFromReactAppListener = (message, sender, response) => {
-    console.log('[content.js]. Message received', {
-        message,
-        sender,
-    })
-}
+  console.log('[content.js]. Message received', {
+    message,
+    sender,
+  });
+};
 /**
  * Fired when a message is sent from either an extension process or a content script.
  */
-chrome.runtime.onMessage.addListener(messagesFromReactAppListener)
+chrome.runtime.onMessage.addListener(messagesFromReactAppListener);
